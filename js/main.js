16a39eb90606e531f55a2b02b14f51335a983757
Teri Ke <32941345+friedforest@users.noreply.github.com>
2018-11-10 09:51:49 -0500

    setup router

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
+Vue.use(VueRouter);
+Vie.use(VeeValidate);
+
+const routes = [{
+        path: '/',
+        component: 'testing'
+  }, {
+        path: '/reader',
+        components: 'reader'
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
 new Vue({
-  el: '#app'
+  el: '#app',
+  watch: {},
+  data: { 
+    msg: 'Hello, I"m testing' 
+  },
+  methods: {},
+  router
 })
